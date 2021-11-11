d219c16b39293137d08e0118da52b56a14c72530
Michael Walker <michaelwwalker42@gmail.com>
2021-11-10 18:17:38 -0700

    added number input to shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -160,29 +160,25 @@ else {
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 'REFILL','UPGRADE', OR 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for 'REFILL',2 for 'UPGRADE', or 3 for 'LEAVE' to make a choice."
   );
+//convert strings to intergers with parseInt()
+  shopOptionPrompt = parseInt(shopOptionPrompt);
 
   // use switch to carry out action
   switch (shopOptionPrompt) {
-    case "refill":
-    case "REFILL":
+    case 1:
       playerInfo.refillHealth();
       break;
-    case "upgrade":
-    case "UPGRADE":
+    case 2:
       playerInfo.upgradeAttack();
       break;
-
-    case "leave":
-    case "LEAVE":
-            window.alert("Leaving the store.");
-
+    case 3:
+      window.alert("Leaving the store.");
       //do nothing, so function will end
       break;
     default:
       window.alert("you did not pick a valid option. Try again.");
-
       // call shop() again to force player to pick a valid option
       shop();
       break;
